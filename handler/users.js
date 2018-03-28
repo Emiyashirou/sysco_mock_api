11642d14bb9b0a5a04ae881a7814c774784dcbf6
lei.li <lei.li@slalom.com>
2018-03-28 11:21:05 -0500

    Add APIs, fill in readme

---
+++ handler/users.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var fs = require("fs");
+
+exports.handler = (event, context, callback) => {
+
+  let id = (event.pathParameters || {}).id || false;
+
+  if(id){
+    switch (event.httpMethod) {
+      case "GET":
+        var users_json = JSON.parse(fs.readFileSync("mock_data/users.json"));
+        var spe_user = {};
+        users_json.data.map(user => {
+          if(user.Role.LookupId.toString() === id){
+            spe_user = user;
+          }
+        });
+        callback(null, {body: JSON.stringify({data: spe_user}), statusCode: 200});
+        break;
+      case "POST":
+        callback(null, {statusCode: 200});
+        break;
+      default:
+        console.log("Error: unsupported HTTP method (" + event.httpMethod + ")");
+        callback(null, {statusCode: 501});
+    }
+  } else {
+      switch (event.httpMethod) {
+        case "GET":
+          var users_json = JSON.parse(fs.readFileSync("mock_data/users.json"));
+          callback(null, {body: JSON.stringify(users_json), statusCode: 200});
+          break;
+        case "PUT":
+          callback(null, {statusCode: 200});
+          break;
+      }
+  }
+}
